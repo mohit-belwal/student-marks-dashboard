4cbaeb8c576ffcb1fcf753f1c986207885aa1c12
Mohit Belwal <75572435+mohit-belwal@users.noreply.github.com>
2021-05-26 13:36:01 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('./database/mongoose.js')
+const path= require('path')
 const studentRouter = require('./routers/route')
 
 const app = express()
@@ -16,9 +17,8 @@ const PORT = process.env.PORT || 5000
 
 if(process.env.NODE_ENV == "production"){
     app.use(express.static('client/build'));
-    const path= require("path");
     app.get("*",(req, res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+        res.sendFile(path.join(__dirname+'/../client/build/index.html'));
     })
 }
 
